eb2a3e99915d28181bd6b326d2402ec0161ce18d
Donatello Fantauzzi <donatello.fantauzzi@gmail.com>
2025-07-12 00:05:33 +0200

    Added supabase settings

---
+++ pages/test-supabase.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Pagina test supabase</h1>
+</template>
+<script setup>
+const { $supabase } = useNuxtApp();
+
+onMounted(async () => {
+  const { data, error } = await $supabase.auth.getSession();
+  console.log("Sessione attuale:", data, error);
+});
+</script>
