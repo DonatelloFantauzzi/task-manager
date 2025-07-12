e6a9f5ee6bb51782071e4159256ca4edf22bc986
Donatello Fantauzzi <donatello.fantauzzi@gmail.com>
2025-07-12 17:32:46 +0200

    Created middleware for protection routes

---
+++ middleware/auth.js
@@ -0,0 +1,7 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { $supabase } = useNuxtApp();
+  const { data } = await $supabase.auth.getSession();
+  if (!data.session) {
+    return navigateTo("/login");
+  }
+});
