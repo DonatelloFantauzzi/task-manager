d738833441e5a66dd92b2aa72326a9c324449abc
Donatello Fantauzzi <donatello.fantauzzi@gmail.com>
2025-07-12 17:33:35 +0200

    added console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const { $supabase } = useNuxtApp();
   const { data } = await $supabase.auth.getSession();
+  console.log(data);
   if (!data.session) {
     return navigateTo("/login");
   }
